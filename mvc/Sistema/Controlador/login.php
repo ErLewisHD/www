a100fc0f7a6a5716604e4bd0a20f1100faebaf57
AntonioBascu <antoniobascu98@gmail.com>
2020-12-03 21:05:48 +0100

    login controlador y modelo

--- mvc/Sistema/Controlador/login.php
+++ mvc/Sistema/Controlador/login.php
@@ -1,7 +1,11 @@
 <?php
-require '../Modelo/login.php';
-$login= new login_modelo();
+require '../Modelo/modelo.php';
+$login= new Cliente();
 $usuario = $login->getUsuario($_POST["dni"]);
+$salida = 1;
+if($usuario != 1 && $usuario != 2){
+  $salida = $login->comprobarContraseña($_POST["password"]);
+}
 require '../Vista/errorLogin.php';
 
 ?>
