e44dd73b5c0a7b4934ec956bde79d3aa6d3f977d
JavierJH99 <JavierJH99@users.noreply.github.com>
2020-12-04 13:17:04 +0100

    Recatpcha Eliminado

--- mvc/Sistema/Modelo/registro.php
+++ mvc/Sistema/Modelo/registro.php
@@ -9,12 +9,6 @@
 require './conexion.php';
 require "../../lib/recaptchalib.php";
 
-	//Recaptcha
-	$secret = "6LdSM-0ZAAAAALAlk2RiAsoPi96q6KykzerpHtg_";
-	$response = null;
-	$reCaptcha = new ReCaptcha($secret);
-
-
 	//Campos para registro
 	$conexion->set_charset('utf8'); //establece el conjunto de caracteres en la conexión, para que no haya problema de acentos y ñ de los campos
 	$pass = sha1($_POST["password"]);
@@ -24,7 +18,7 @@ require "../../lib/recaptchalib.php";
 	if (!$resultado) {
 		echo "<h1>¡¡ERROR FATAL!!</h1>";
 		echo "<h2>Algo no ha ido bien</h2>";
-	die("No se puede realizar el registro (error numero:<b>$conexion->errno</b>): <br><b>$conexion->error</b>");
+		die("No se puede realizar el registro (error numero:<b>$conexion->errno</b>): <br><b>$conexion->error</b>");
 	}
 	else{
 		echo "<h1>¡¡Bienvenido/a "; echo $_POST["nombre"]; echo "!!</h1><br>";
