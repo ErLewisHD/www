72dcbab2e00717504b8dd463c69b1375f6836944
JavierJH99 <JavierJH99@users.noreply.github.com>
2020-12-14 18:19:24 +0100

    Arreglos

--- mvc/Sistema/Vista/ramos1.php
+++ mvc/Sistema/Vista/ramos1.php
@@ -243,17 +243,17 @@ function normalImg(x) {
     for (i = 0; i < x.length; i++) {
         table += '<tr><td><br><h5>' +
         '<form type="hidden" action="pago.php" method="post" class="login-form">' +
-        x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue + '</h5>Color: ' +
-        '<input type = "hidden" name = "nombre" value = "x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue" />' +
-        x[i].getElementsByTagName("color")[0].childNodes[0].nodeValue + '<br>Cantidad disponible: ' +
-        '<input type = "hidden" name = "color" value = "x[i].getElementsByTagName("color")[0].childNodes[0].nodeValue" />' +
-        x[i].getElementsByTagName("cantidad")[0].childNodes[0].nodeValue + '<br>Precio: ' +
-        '<input type = "hidden" name = "cantidad" value = "x[i].getElementsByTagName("cantidad")[0].childNodes[0].nodeValue" />' +
-        x[i].getElementsByTagName("precio")[0].childNodes[0].nodeValue + '€' +
-        '<input type = "hidden" name = "precio" value = "x[i].getElementsByTagName("precio")[0].childNodes[0].nodeValue" />' +
-        ' Iva aplicado: ' + x[i].getElementsByTagName("iva")[0].childNodes[0].nodeValue + '%' +
-        '<input type = "hidden" name = "iva" value = "x[i].getElementsByTagName("iva")[0].childNodes[0].nodeValue" />' +
-        '<br><br><input type="submit" value="Comprar"/>' +
+          x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue + '</h5>Color: ' +
+          '<input type = "hidden" name = "nombre" value = "x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue" />' +
+          x[i].getElementsByTagName("color")[0].childNodes[0].nodeValue + '<br>Cantidad disponible: ' +
+          '<input type = "hidden" name = "color" value = "x[i].getElementsByTagName("color")[0].childNodes[0].nodeValue" />' +
+          x[i].getElementsByTagName("cantidad")[0].childNodes[0].nodeValue + '<br>Precio: ' +
+          '<input type = "hidden" name = "cantidad" value = "x[i].getElementsByTagName("cantidad")[0].childNodes[0].nodeValue" />' +
+          x[i].getElementsByTagName("precio")[0].childNodes[0].nodeValue + '€' +
+          '<input type = "hidden" name = "precio" value = "x[i].getElementsByTagName("precio")[0].childNodes[0].nodeValue" />' +
+          ' Iva aplicado: ' + x[i].getElementsByTagName("iva")[0].childNodes[0].nodeValue + '%' +
+          '<input type = "hidden" name = "iva" value = "x[i].getElementsByTagName("iva")[0].childNodes[0].nodeValue" />' +
+          '<br><br><input type="submit" value="Comprar"/>' +
       '</form>' +
       '</td><td><img src="../../img/' + x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue + '" alt="Foto" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width="200" height="200"> </td></tr>';
     }
