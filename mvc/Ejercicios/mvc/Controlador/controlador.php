4112bdb1a732790124967c17df47bd17bdfc30c0
JavierJH99 <JavierJH99@users.noreply.github.com>
2020-12-16 19:52:14 +0100

    FIn

--- mvc/Ejercicios/mvc/Controlador/controlador.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require '../Modelo/modelo.php';
-  function obtenerRegistros($DNI){
-    $registros = new Funciones();
-    $resultado = $registros->ejercicio2($DNI);
-    return $resultado;
-  }
-?>
